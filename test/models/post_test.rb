2f901d54a94084abcdd5d38c00bf4a353f91c731
Gregorio Galante <me@gregoriogalante.com>
2025-09-16 12:53:20 +0200

    Added some tests + update background image

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "formatting dei tags nel before_validation" do
+    post = Post.new(tags: "ruby, rails, #test, rails")
+    post.valid?
+    assert_equal "#ruby #rails #test", post.tags
+  end
+
+  test "scope search trova post per content" do
+    post = Post.create!(content: "Questo è un post di test", tags: "#test")
+    results = Post.search("test")
+    assert_includes results, post
+  end
+
+  test "scope search trova post per tags" do
+    post = Post.create!(content: "Altro post", tags: "#speciale")
+    results = Post.search("speciale")
+    assert_includes results, post
+  end
+
+  test "image_thumb e image_large restituiscono variant" do
+    post = Post.create!(content: "Questo è un post di test", tags: "#test")
+    post.image.attach(io: File.open(Rails.root.join("test/fixtures/files/image.png")), filename: "image.png", content_type: "image/png")
+    assert_not_nil post.image_thumb
+    assert_not_nil post.image_large
+  end
 end
--- test/models/presentation_test.rb
+++ test/models/presentation_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 class PresentationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @project = Project.create!(name: "Test Project", year: 2024)
+    @presentation = Presentation.new(
+      name: "Presentazione di test",
+      width_px: 800,
+      height_px: 600,
+      project: @project,
+    )
+  end
+
+  test "should be valid with valid attributes" do
+    assert @presentation.valid?
+  end
+
+  test "should not be valid without name" do
+    @presentation.name = nil
+    assert_not @presentation.valid?
+  end
+
+  test "should not be valid with name longer than 50 chars" do
+    @presentation.name = "a" * 51
+    assert_not @presentation.valid?
+  end
+
+  test "should not be valid without width_px" do
+    @presentation.width_px = nil
+    assert_not @presentation.valid?
+  end
+
+  test "should not be valid without height_px" do
+    @presentation.height_px = nil
+    assert_not @presentation.valid?
+  end
+
+  test "shareable? returns true if share_code is present" do
+    @presentation.share_code = "abc123"
+    assert @presentation.shareable?
+  end
+
+  test "shareable? returns false if share_code is nil" do
+    @presentation.share_code = nil
+    assert_not @presentation.shareable?
+  end
+
+  test "import returns false if images is nil" do
+    assert_not @presentation.import(nil)
+  end
+
+  # Puoi aggiungere altri test per il metodo import se necessario, ad esempio con immagini mockate
 end
