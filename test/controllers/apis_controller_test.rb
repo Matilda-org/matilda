47b4df47118ed13209324c7eabdd595559f66e16
Gregorio Galante <me@gregoriogalante.com>
2025-09-16 21:54:42 +0200

    Added some tests

---
+++ test/controllers/apis_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class ApisControllerTest < ActionDispatch::IntegrationTest
+end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -21,4 +21,26 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_not_nil @controller.instance_variable_get(:@projects)
     assert_not_nil @controller.instance_variable_get(:@projects_preferred)
   end
+
+  # Test for GET index without valid policy
+  test "should not get index without valid policy" do
+    get :index
+    assert_redirected_to root_path
+  end
+
+  # Test for GET show with valid policy
+  test "should get show with valid policy" do
+    @user.users_policies.create!(policy: "projects_show")
+    project = projects(:one)
+    get :show, params: { id: project.id }
+    assert_response :success
+    assert_not_nil @controller.instance_variable_get(:@project)
+  end
+
+  # Test for GET show without valid policy
+  test "should not get show without valid policy" do
+    project = projects(:one)
+    get :show, params: { id: project.id }
+    assert_redirected_to root_path
+  end
 end
