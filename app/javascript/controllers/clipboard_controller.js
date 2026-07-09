b992540a4bbedaf33547f27490dc72ded7569c2b
Gregorio Galante <me@gregoriogalante.com>
2026-07-09 13:21:30 +0200

    UI Updates

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,5 +3,19 @@ import Clipboard from 'stimulus-clipboard'
 export default class extends Clipboard {
   connect() {
     super.connect()
+    // preserve original markup (base lib uses innerText, dropping icons)
+    if (this.hasButtonTarget) this.originalHTML = this.buttonTarget.innerHTML
+  }
+
+  // override to render HTML success content (icons) instead of plain text
+  copied() {
+    if (!this.hasButtonTarget) return
+
+    if (this.timeout) clearTimeout(this.timeout)
+
+    this.buttonTarget.innerHTML = this.data.get('successContent')
+    this.timeout = setTimeout(() => {
+      this.buttonTarget.innerHTML = this.originalHTML
+    }, this.successDurationValue)
   }
 }
